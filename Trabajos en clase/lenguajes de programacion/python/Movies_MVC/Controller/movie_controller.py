1ddbde743a88006e753f26d766cce285bcea30e8
Hilan Eduardo Alegria Yate <hilan.alegria00@usc.edu.co>
2025-10-16 20:31:40 -0500

    Add files via upload

--- Trabajos en clase/lenguajes de programacion/python/Movies_MVC/Controller/movie_controller.py
+++ Trabajos en clase/lenguajes de programacion/python/Movies_MVC/Controller/movie_controller.py
@@ -7,12 +7,12 @@ class MovieController:
     
     def __init__(self, repository: MovieRepository, view: MovieView):
         self.repository = repository
-        self.view = view
+        self.view = MovieView
         
     # implementacion de una funcion de CRUD que seria llamada desde la vista
     # O le reporaria a la vista
-    
     def listAllMovies(self):
         moviesData = self.repository.get_all()
         # visualiza la informacion a travez del view:
         self.view.showMovies(moviesData)
+        
